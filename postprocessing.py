fc67b13d6e545b7a250d0408d5ff9582ad148bf2
ahryncyszyn <ahryncyszyn11@gmail.com>
2024-04-21 19:37:30 +0200

    first commit

---
+++ postprocessing.py
@@ -0,0 +1,11 @@
+
+
+def postprocessing(results_files):
+    """ Extracts the total energy from each ".out" ORCA file. Creates two files:
+    "data" - containing all of the molecules in xyz format
+    "labels" - containing all of the corresponding energies in the same order
+
+    results_files (str): path to the folder created by "inputs_generation.py", 
+                         contains all calculations and their results
+    """
+    pass

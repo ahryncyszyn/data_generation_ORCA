d0f813da5824a6d4b16809fb521e9e62db5a95a6
ahryncyszyn <ahryncyszyn11@gmail.com>
2024-04-22 16:43:55 +0200

    wrote first version of the code

---
+++ generate_inputs.py
@@ -0,0 +1,60 @@
+import os
+
+from sample_data import sample_data
+from xyz_utils import exportXYZ_with_charge
+
+def generate_inputs(xyz_files_dir, output_dir, no_batches):
+    """ Samples the data from the dataset and creates ORCA input file for each one of them.
+    xyz_files_dir (str): path to the dataset
+    output_dir (str): path where to store the results
+    no_batches (int): number of batches (molecules per each datatype) to sample
+    """
+
+    # create the output directory if it doesn't exist yet
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # sample the data
+    # data_points -> list[(coords, atoms, charge, info)]
+    elements_all, coords_all, charges_all, names_all = sample_data(xyz_files_dir, no_batches)
+    assert len(coords_all) == len(elements_all) == len(charges_all)
+    
+    # save sampled data
+    os.chdir(output_dir)
+    #for i in range(no_molecules):
+    for i, (elements, coords, charge, name) in enumerate(zip(elements_all, coords_all, charges_all, names_all)):
+        print(f"Saving data for {name}, {coords}, {elements}, charge: {charge}")
+
+        # create a folder for storing calculations of one molecule
+        temp_dir_name = f"inp_{i}_{name}"
+        temp_dir = os.path.join(output_dir, temp_dir_name)
+        os.makedirs(temp_dir)
+        os.chdir(temp_dir)
+
+        # create data file with xyz coords
+        exportXYZ_with_charge(elements, coords, charge, "molecule.inp")
+
+        # create ORCA input
+        ORCA_input_file(elements, coords, charge)
+        
+    print(f"Input ORCA files have been generated for {len(coords_all)} molecules")
+
+
+
+def ORCA_input_file(elements, coords, charge):
+    
+    with open("molecule.inp", "w") as f:
+        f.write("! DLPNO-CCSD(T) cc-pVTZ cc-pVTZ/C TIGHTSCF\n\n")
+        f.write("%pal\n")
+        f.write("nprocs 8\n")
+        f.write("end\n\n")
+        f.write("%maxcore 2000\n")
+
+        #multiplicity set to 2, as all molecules are radicals
+        f.write(f"* xyz {charge} 2")
+
+        for element, coord in zip(elements, coords):
+            f.write(f"{element.capitalize()} {coord[0]} {coord[1]} {coord[2]}\n")
+
+
+    

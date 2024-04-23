09ee11099fd82d811271e762db9b6029c3ea34b7
ahryncyszyn <ahryncyszyn11@gmail.com>
2024-04-23 16:29:08 +0200

    created example script for inputs generation

---
+++ scripts/inputs_generation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=cpuonly
+#SBATCH --time=3-00:00:00
+#SBATCH --nodes=1
+#SBATCH --output=orcainputs%j.out
+#SBATCH --error=orcainputs%j.error
+#SBATCH --job-name=orcainputs
+
+dataset_dir=path
+output_dir=path
+no_batches=1 
+
+python generate_inputs "$dataset_dir" "$output_dir" $no_batches
